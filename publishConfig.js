c855e12331109d94541242953de44e600be9deaa
zp <zhoup.top>
2026-05-29 22:46:20 +0800

    v1.0.1

--- publishConfig.js
+++ publishConfig.js
@@ -19,7 +19,7 @@ module.exports = {
         'test',
     ],
     excludesAnyPath: [], // includes 匹配到则跳过
-    remoteServerPath: 'admin@192.168.10.20:/home/admin/Desktop/project/view-img', // 远程服务器的地址和目录
+    remoteServerPath: 'rpi:/home/admin/Desktop/project/view-img', // 远程服务器的地址和目录
     localDist: 'dist',  // 上传之前暂存文件的目录, 每次上传成功后会自动删除, 上传失败则会保留
     versionFile: 'public/v.txt', // 打包时会将打包时间和git提交id写入这个文件中, 这个文件会上传到remoteServerPath的路径后面, 如果不需要生成版本文件可以注释此项
     md5CacheFile: 'md5Cache.json', // 增量上传会计算每个文件的md5并且保存到这个文件中, 只上传md5不一致的文件, 如果需要每次都全量上传可以注释此项
